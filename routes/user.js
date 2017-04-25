cfc2d5509bc6e4ccabf80bf85d96cc4c0f35a3a8
kenNg1 <kng1230@hotmail.com>
2017-04-25 15:57:43 +0800

    updated product schema and admin.js

--- routes/user.js
+++ routes/user.js
@@ -34,6 +34,7 @@ router.post('/signup', function(req,res,next){
   user.profile.name = req.body.name;
   user.email = req.body.email;
   user.password = req.body.password;
+  user.profile.picture = user.gravatar();
 
   User.findOne({ email: req.body.email } , function(err, existingUser) {
     if(existingUser) {
@@ -42,8 +43,11 @@ router.post('/signup', function(req,res,next){
     } else {
       user.save(function(err,user) {
         if (err) return next(err);
-
-        return res.redirect('/');
+        //explained vid 27 @ 7min 49
+        req.logIn(user,function(err){
+          if(err) return next(err);
+          res.redirect('/profile');
+        })
       });
     }
   });
@@ -52,6 +56,26 @@ router.post('/signup', function(req,res,next){
 router.get('/logout',function(req,res,next){
   req.logout();
   res.redirect('/');
+});
+
+router.get('/edit-profile',function(req,res,next){
+  res.render('accounts/edit-profile.ejs',{message: req.flash('success')});
+});
+
+router.post('/edit-profile',function(req,res,next){
+  User.findOne({_id:req.user._id}, function(err,user){
+
+    if(err) return next(err);
+
+    if(req.body.name) user.profile.name = req.body.name;
+    if(req.body.address) user.address = req.body.address;
+
+    user.save(function(err){
+      if(err) return next(err);
+      req.flash('success','Successfully Edited your profile');
+      return res.redirect('/edit-profile');
+    });
+  });
 });
 
 module.exports = router;
