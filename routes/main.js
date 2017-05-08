bf58f03b272a51a3547196777ac3a3aa37dd399f
kenNg1 <kng1230@hotmail.com>
2017-05-08 17:21:44 +0800

    implement pagination

--- routes/main.js
+++ routes/main.js
@@ -4,6 +4,27 @@ var Product = require('../models/product')
 
 //router or app is acceptable
 
+function paginate(req, res, next) {
+  var perPage = 9;
+  var page = req.params.page;
+
+  Product
+  .find()
+  .skip( perPage * page )
+  .limit( perPage )
+  .populate('category')
+  .exec(function(err,products){
+    if (err) return next(err);
+    Product.count().exec(function(err,count){
+      if (err) return next(err);
+      res.render('main/product-main', {
+        products: products,
+        pages: count / perPage
+      });
+    });
+  });
+}
+
 //creating a bridge (connection) btwn product database and elastic search
 Product.createMapping(function(err, mapping) {
   if (err) {
@@ -52,13 +73,22 @@ router.get('/search', function(req,res,next){
   }
 });
 
-router.get('/', function(req,res){
-  res.render('main/home');
+router.get('/', function(req,res, next){
+
+  if (req.user) {
+    paginate(req,res,next);
+  } else {
+    res.render('main/home');
+  }
+});
+
+router.get('/page/:page', function(req,res,next){
+  paginate(req,res,next);
 });
 
 router.get('/about', function(req,res){
   res.render('main/about');
-});
+  });
 
 router.get('/products/:id', function(req,res,next){
   Product
