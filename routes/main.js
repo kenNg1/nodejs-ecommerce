45935302fe7b8c50d36fb1cf519c3a181644d26f
kenNg1 <kng1230@hotmail.com>
2017-05-08 08:48:25 +0800

    add search functionality, npm install --save mongoosastic

--- routes/main.js
+++ routes/main.js
@@ -4,6 +4,54 @@ var Product = require('../models/product')
 
 //router or app is acceptable
 
+//creating a bridge (connection) btwn product database and elastic search
+Product.createMapping(function(err, mapping) {
+  if (err) {
+    console.log("error creating mapping");
+    console.log(err);
+  } else {
+    console.log("mapping created");
+    console.log(mapping);
+  }
+});
+
+var stream = Product.synchronize();
+var count = 0;
+
+stream.on('data', function() {
+  count++;
+});
+
+stream.on('close', function() {
+  console.log("Indexed" + count + " documents")
+});
+
+stream.on('error', function(err) {
+  console.log(err);
+});
+
+router.post('/search', function(req,res,next) {
+  res.redirect('/search?q=' + req.body.q);
+});
+
+router.get('/search', function(req,res,next){
+  if(req.query.q) {
+    Product.search({
+      query_string: { query: req.query.q }
+    }, function(err,results){
+      results:
+      if (err) return next(err);
+      var data = results.hits.hits.map(function(hit){
+        return hit;
+      });
+      res.render('main/search-result', {
+        query: req.query.q,
+        data: data
+      })
+    });
+  }
+});
+
 router.get('/', function(req,res){
   res.render('main/home');
 });
