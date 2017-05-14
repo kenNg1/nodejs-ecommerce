0f52443a8569499371981c2bef5a5a18d87a313e
kenNg1 <kng1230@hotmail.com>
2017-05-14 13:24:54 +0800

    amend html to show search results, can potentially be improved by showing suggested words

--- public/js/custom.js
+++ public/js/custom.js
@@ -16,17 +16,25 @@ $(function(){
           return hit;
         });
 
-        console.log(data);
+        $("#searchResults").empty();
+        for (var i = 0; i < data.length; i++) {
+          var html = "";
+          html += '<div class="col-md-4">';
+          html += '<a href="/product/'+ data[i]._source._id + '">';
+          html += '<div class="thumbnail">';
+          html += '<img src="'+ data[i]._source.image + '" alt="">'
+          html += '<div class="caption">';
+          html += '<h3>'+ data[i]._source.name +'</h3>';
+          html += '<p>'+ data[i]._source.category.name +'</p>';
+          html += '<p>'+ data[i]._source.price + '</p>';
+          html += '</div></div></a></div>'
+
+          $('#searchResults').append(html);
+        }
       },
-
       error: function(error) {
-        console.log(err);
+        console.log(error);
       }
-    })
-  })
-
-
-
-
-
-})
+    });
+  });
+});
